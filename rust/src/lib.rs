06dda651ddb99853fe7bdd658562e7441d248515
williammmm <me@williammmm.dev>
2025-03-23 20:34:34 +0000

    updated rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,30 @@
+//! # Elon sort
+//! 
+//! Will delete items at random, then declare that the list is sorted, regardless of whether or not it is.
+//! 
+//! ##### Advantages
+//! - uhh
+//! - ummm
+//! - ahhhh
 use rand::{rng, Rng};
 
-
-
-///
+/// I  feel like this doesnt need much explanation, this existes purely as a joke
+/// 
+/// It will flip a coin for each element and remove them half the time
+/// 
+/// # Examples
+/// ```
+/// use elon_sort::elon_sort;
+/// // create a vector to 'sort'
+/// let mut vector = vec![1,3,5,2,4,6];
+/// 
+/// elon_sort(&mut vector);
+/// 
+/// println!("{:?}",vector);
+/// // tada, you will see something like this
+/// // [1,5,4] or
+/// // [1,3,5,6] (it is sometimes right)
+/// ```
 pub fn elon_sort<T>(array: &mut Vec<T>) {
     let mut rng = rng();
     let mut removals: Vec<usize> = Vec::new();
